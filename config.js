644c48199167f538142dd965b4b8194d8cf6f72a
AMTourky <abdelhameed.tourky@radius60.com>
2016-01-20 00:53:22 +0200

    use ssh URL instead of HTTPS of the sample project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.config = {
     Sample: {
-        gitRepoURL: "https://github.com/AMTourky/CocoaBindingReusableViews.git"
+        gitRepoURL: "git@github.com:AMTourky/CocoaBindingReusableViews.git"
     },
     serverTimeoutInMins: 60,
     isHTTPS: false,
